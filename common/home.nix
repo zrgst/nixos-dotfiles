e03a89206adbc3fb88485b2f345565f3a9065fae
Zrgst <zrgst@protonmail.com>
2026-02-17 16:27:56 +0100

    moved polkit to common

--- common/home.nix
+++ common/home.nix
@@ -24,6 +24,27 @@ in
     };
   };
 
+  # Dette starter Polkit-agenten automatisk ved innlogging
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    Unit = {
+      Description = "gnome-polkit-authentication-agent-1";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
   programs.bash = {
     enable = true;
     shellAliases = {
--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -43,20 +43,6 @@
     ];
   };
 
-  # Polkit auth agent
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    description = "gnome-polkit-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
 
   # --- NETTVERK & SSH --- #
   networking.hostName = "desktop-zrgst";
